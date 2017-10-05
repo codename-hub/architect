93bf5de64341390313864a0d2a5bf58f7757f856
Kevin Dargel <kevin@jocoon.de>
2017-10-05 17:07:05 +0200

    updated composer.json and added empty app class file

---
+++ backend/class/app.php
@@ -0,0 +1,12 @@
+<?php
+namespace codename\architect;
+
+/**
+ * app class for a the architect app
+ * @package architect
+ * @author Kevin Dargel
+ * @since 2017-10-05
+ */
+class app extends \codename\core\app {
+
+}
