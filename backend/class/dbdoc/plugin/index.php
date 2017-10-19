45d7ed45091591915cada7ce41fe26a74f23357a
Kevin Dargel <kevin@jocoon.de>
2017-10-19 17:10:12 +0200

    dbdoc: added abstract index plugin

---
+++ backend/class/dbdoc/plugin/index.php
@@ -0,0 +1,24 @@
+<?php
+namespace codename\architect\dbdoc\plugin;
+
+/**
+ * we may add some kind of loading prevention, if some classes are not loaded/undefined
+ * as we're using a filename that is the same as standard php scripts loaded for directories
+ * if none is given
+ */
+
+/**
+ * plugin for providing and comparing index / indices field config in a model
+ * @package architect
+ */
+abstract class index extends \codename\architect\dbdoc\plugin {
+
+  /**
+   * @inheritDoc
+   */
+  public function getDefinition()
+  {
+    return $this->adapter->config->get('index');
+  }
+
+}
