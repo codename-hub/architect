f0d112787e0355c7ed6afcf4f752983f4fb9ea1f
Kevin <kevin@jocoon.de>
2018-08-14 14:03:59 +0200

    [JIRA CODENAME-237] disabled old behaviour for collections, as we now do it in completely different fashion. Hey, yes. Fashion!

--- backend/class/dbdoc/plugin/sql/collection.php
+++ backend/class/dbdoc/plugin/sql/collection.php
@@ -30,33 +30,37 @@ class collection extends \codename\architect\dbdoc\plugin\collection {
 
     $precededBy = [];
 
-    foreach($collectionDefinitions as $def) {
-      
-      // $foreignAdapter = $this->adapter->dbdoc->getAdapter($def['schema'], $def['model'], $def['app'] ?? '', $def['vendor'] ?? '');
-      // $plugin = $foreignAdapter->getPluginInstance('table', [], $this->virtual /*, array('field' => $def['key'])*/ );
-      // if($plugin != null) {
-      //   $precededBy[] = $plugin->getTaskIdentifierPrefix();
-      // }
+    // 
+    // TODO: Check, if the given collection config is correct
+    //
 
-      $aux = $def['aux'];
-      $auxAdapter = $this->adapter->dbdoc->getAdapter($aux['schema'], $aux['model'], $aux['app'] ?? '', $aux['vendor'] ?? '');
-      $plugin = $auxAdapter->getPluginInstance('table', [], $this->virtual /*, array('field' => $aux['key'])*/);
-      if($plugin != null) {
-        $precededBy[] = $plugin->getTaskIdentifierPrefix();
-      } else {
-        echo("table plugin is null");
-      }
-
-      print_r($precededBy);
-
-      $tasks[] = $this->createTask(task::TASK_TYPE_REQUIRED, "DUMMY_COLLECTION_TASK_RUN",
-        array(
-          // 'field' => $field,
-          'config' => $def,
-        ),
-        $precededBy
-      );
-    }
+    // foreach($collectionDefinitions as $def) {
+    //
+    //   // $foreignAdapter = $this->adapter->dbdoc->getAdapter($def['schema'], $def['model'], $def['app'] ?? '', $def['vendor'] ?? '');
+    //   // $plugin = $foreignAdapter->getPluginInstance('table', [], $this->virtual /*, array('field' => $def['key'])*/ );
+    //   // if($plugin != null) {
+    //   //   $precededBy[] = $plugin->getTaskIdentifierPrefix();
+    //   // }
+    //
+    //   $aux = $def['aux'];
+    //   $auxAdapter = $this->adapter->dbdoc->getAdapter($aux['schema'], $aux['model'], $aux['app'] ?? '', $aux['vendor'] ?? '');
+    //   $plugin = $auxAdapter->getPluginInstance('table', [], $this->virtual /*, array('field' => $aux['key'])*/);
+    //   if($plugin != null) {
+    //     $precededBy[] = $plugin->getTaskIdentifierPrefix();
+    //   } else {
+    //     echo("table plugin is null");
+    //   }
+    //
+    //   print_r($precededBy);
+    //
+    //   $tasks[] = $this->createTask(task::TASK_TYPE_REQUIRED, "DUMMY_COLLECTION_TASK_RUN",
+    //     array(
+    //       // 'field' => $field,
+    //       'config' => $def,
+    //     ),
+    //     $precededBy
+    //   );
+    // }
 
     return $tasks;
   }
