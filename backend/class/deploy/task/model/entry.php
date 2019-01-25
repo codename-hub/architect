72677761077d8c03596b22e178b7b8e50f3cfbc9
Kevin <kevin@jocoon.de>
2019-01-25 13:45:36 +0100

    provide task model_entry for adding/updating model entries by unique keys/pkeys

--- backend/class/deploy/task/model/entry.php
+++ backend/class/deploy/task/model/entry.php
@@ -1,12 +1,23 @@
 <?php
 namespace codename\architect\deploy\task\model;
 
+use codename\architect\deploy\taskresult;
+
+use codename\core\exception;
 
 /**
- * base class for doing model-specific tasks
+ * class for managing a model entry
  */
-class query extends \codename\architect\deploy\task\model {
+class entry extends \codename\architect\deploy\task\model {
 
+  /**
+   * @inheritDoc
+   */
+  protected function handleConfig()
+  {
+    parent::handleConfig();
+    $this->data = $this->config->get('data');
+  }
   /**
    * @inheritDoc
    */
@@ -14,8 +25,87 @@ class query extends \codename\architect\deploy\task\model {
   {
     $model = $this->getModelInstance();
 
-    return new \codename\architect\deploy\taskresult\text([
-      'text' => print_r($res, true)
+    $normalizedData = $model->normalizeData($this->data);
+
+    $model->validate($normalizedData);
+
+    $text = '';
+
+    if(count($errors = $model->getErrors()) > 0) {
+      $text = "Model data validation error: " . print_r($errors, true);
+    } else {
+
+      // check for PKEY value or uniques
+      if($normalizedData[$model->getPrimarykey()] ?? false) {
+
+        // we have a primary key - update the whole dataset
+        $model->save($normalizedData);
+        $text = 'Model saved via PKEY';
+
+      } else {
+        if($model->getConfig()->get('unique')) {
+          $filtersAdded = false;
+          foreach($model->getConfig()->get('unique') as $uniqueKey) {
+            if(is_array($uniqueKey)) {
+              // multiple keys, combined unique key
+              $filters = [];
+              foreach($uniqueKey as $key) {
+                if($normalizedData[$key] ?? false) {
+                  $filters[] = [ 'field' => $key, 'operator' => '=', 'value' => $normalizedData[$key]];
+                } else {
+                  // irrelevant unique key, one value is null
+                  $filters = [];
+                  break;
+                }
+              }
+              if(count($filters) > 0) {
+                $filtersAdded = true;
+                $model->addFilterCollection($filters, 'AND');
+              }
+            } else {
+              // single unique key field
+              $filtersAdded = true;
+              $model->addFilter($uniqueKey, $normalizedData[$uniqueKey]);
+            }
+          }
+
+          if($filtersAdded) {
+            $res = $model->search()->getResult();
+            if(count($res) === 1) {
+              // update using found PKEY
+              $normalizedData[$model->getPrimarykey()] = $res[0][$model->getPrimarykey()];
+              $model->save($normalizedData);
+              $text = 'Model saved via filter, updated';
+            } else if(count($res) === 0) {
+              //
+              // NOTE/WARNING:
+              // if you have a PKEY or UNIQUE key constraint values differing
+              // (two datasets which could match the filter)
+              // we're trying to save here
+              // which will cause an error.
+              //
+              // insert
+              $model->save($normalizedData);
+              $text = 'Model saved via filter, inserted/created.';
+            } else {
+              // error - multiple results
+              throw new exception('EXCEPTION_TASK_MODEL_ENTRY_MULTIPLE_UNIQUE_KEY_RESULTS', exception::$ERRORLEVEL_ERROR, $res);
+            }
+          } else {
+            // no filterable fields as a base
+            throw new exception('EXCEPTION_TASK_MODEL_ENTRY_NO_FILTERABLE_KEYS', exception::$ERRORLEVEL_ERROR, $res);
+          }
+
+        } else {
+          // error, not handleable:
+          // no PKEY and no unique-combination given
+          throw new exception('EXCEPTION_TASK_MODEL_ENTRY_NO_UNIQUE_OR_PRIMARY_KEYS_GIVEN_OR_AVAILABLE', exception::$ERRORLEVEL_ERROR);
+        }
+      }
+    }
+
+    return new taskresult\text([
+      'text' => $text
     ]);
   }
 
